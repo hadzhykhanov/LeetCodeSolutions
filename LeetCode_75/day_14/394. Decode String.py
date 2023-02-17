440df3d326aa5aa31be6868d7444600d82691302
VladGadzikhanov <gadzikenzo@gmail.com>
2023-02-17 15:59:36 +0300

    day_14 added

---
+++ LeetCode_75/day_14/394. Decode String.py
@@ -0,0 +1,32 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+
+        for idx, item in enumerate(s):
+            if item != "]":
+                stack.append(item)
+            else:
+                curr_poped = stack.pop()
+                curr_elements = [curr_poped]
+                curr_digits = []
+
+                while curr_elements[-1] != "[":
+                    curr_poped = stack.pop()
+                    curr_elements.append(curr_poped)
+                else:
+                    curr_elements.pop()
+                    while stack and stack[-1].isdigit():
+                        curr_digit = stack.pop()
+                        curr_digits.append(curr_digit)
+
+                curr_elements.reverse()
+                curr_digits.reverse()
+                curr_st, curr_multiplier = "".join(curr_elements), int("".join(curr_digits))
+
+                stack.append(curr_multiplier * curr_st)
+
+        return "".join(stack)
+
+
+s = Solution()
+print(s.decodeString("3[a2[c]b]"))
