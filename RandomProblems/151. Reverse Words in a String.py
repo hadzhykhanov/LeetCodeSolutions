5446cf2e33e7d39f891b99e276d26c82db4929ba
VladGadzikhanov <gadzikenzo@gmail.com>
2023-03-01 16:28:30 +0300

    Update 151. Reverse Words in a String.py

--- RandomProblems/151. Reverse Words in a String.py
+++ RandomProblems/151. Reverse Words in a String.py
@@ -1,3 +1,26 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
         return " ".join(list(reversed(s.split())))
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        word_starts, word_ends = [], []
+        res = []
+
+        if s[0] != " ":
+            word_starts.append(0)
+
+        for i in range(1, len(s)):
+            if s[i] != " " and s[i - 1] == " ":
+                word_starts.append(i)
+
+            if s[i] == " " and s[i - 1] != " ":
+                word_ends.append(i)
+
+        if s[len(s) - 1] != " ":
+            word_ends.append(len(s))
+
+        for start, end in zip(word_starts, word_ends):
+            res.append(s[start:end])
+
+        return " ".join(list(reversed(res)))
